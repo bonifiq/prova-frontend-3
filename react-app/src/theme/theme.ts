1448212f647bc901fcaead21994f664774b0817d
giihsatiko <giovana_satiko@hotmail.com>
2025-08-15 00:44:06 -0300

    feat: themes and icons

---
+++ react-app/src/theme/theme.ts
@@ -0,0 +1,13 @@
+export const theme = {
+  palette: {
+    neutral: {
+      lightest: '#ffffff'
+    },
+    primary: {
+      lightest: '#7e67a5',
+      regular: '#773cef',
+      dark: '#482484',
+      darkest: '#340055'
+    },
+  },
+};
